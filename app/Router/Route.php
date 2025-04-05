7720727b4df003483a4b0439c0d9c755c5ebcce6
matiassctt <poppydch@gmail.com>
2025-04-05 19:07:07 -0300

    feat: Added comentaries to files

--- app/Router/Route.php
+++ app/Router/Route.php
@@ -1,5 +1,7 @@
 <?php 
 
+// DTO para contener los objetos de la ruta
+
 final readonly class Route
 {
     public function __construct(
@@ -9,14 +11,17 @@ final readonly class Route
         private string $method,
         private array $parameters,
     ) {}
-
+    
+    /**
+     * Metodo que nos permite crear una ruta a partir de un array
+     */
     public static function fromArray(array $data): self
     {
         return new self(
-            $data["name"],
-            $data["url"],
-            $data["controller"],
-            $data["method"],
+            $data["name"] ?? '',
+            $data["url"] ?? '',
+            $data["controller"] ?? '',
+            $data["method"] ?? '',
             $data["parameters"] ?? [],
         );
     }
@@ -36,9 +41,17 @@ final readonly class Route
         return $this->controller;
     }
 
+    /**
+     * Metodo que nos permite, a partir de la ruta de un controlador, obtener el nombre de su clase
+     * Example: src/Domain/PruebaController.php -> PruebaController
+     */
     public function className(): string
     {
+        // Dividimos el nombre completo de la ruta del controllador en un array
+        // Example: src/Domain/PruebaController.php -> ['src', 'Domain', 'PruebaController.php']
         $route = explode("/", $this->controller);
+        // Tomamos la ultima posicion del array y devolvemos su nombre removiendo el .php
+        // Example: ['src', 'Domain', 'PruebaController.php'] -> 'PruebaController.php' -> '.php'
         return str_replace('.php', '', end($route));
     }
 
--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
 
+// Cargamos configuración de composer
 require_once dirname(__DIR__).'/html/vendor/autoload.php';
+// Inicializamos el routeador
 require_once dirname(__DIR__).'/html/app/Router/Routes.php';
+// Inicializamos el autoloader
 require_once dirname(__DIR__).'/html/app/Autoloader/Autoloader.php';
 
+// Utilizamos la libreria 'Dotenv' para cargar nuestros datos
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load(); 
 
+// Cargamos el autoloader
 spl_autoload_register(
     function ($class): void {
         Autoloader::register($class, [
+            // Unicamente la carpeta Service
             "src/Service"
         ]);
     }
 );
 
+// Cargamos el routeador
 $router = startRouter();
 
+// Obtenemos el URL de donde esta entrando el usuario
 $url = $_SERVER["REQUEST_URI"];
 
 try {
+    // A partir del URL y del metodo, el Routeador decide por que ruta entrar
     $router->resolve(
-        $_SERVER["REQUEST_URI"],
+        $url,
         $_SERVER['REQUEST_METHOD']
     );
 } catch (Exception $e) {   
+    // Si la ruta no existe, devolvemos un error 404
     header("HTTP/1.0 404 Not Found");
     echo json_encode([
         "status" => 404,
